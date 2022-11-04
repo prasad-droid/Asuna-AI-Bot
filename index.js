af237afecaa04cc93b9a3a0c7627049a73f7dbaf
Prasad Kalvikatti <65800954+prasad-droid@users.noreply.github.com>
2022-11-04 17:16:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ inp = document.getElementById("input");
 out = document.getElementById("output");
 var text;
 var lat, lon;
+var msg;
+var voices;
 
 apikey = "309fbf4d24ee6e2250d2b188c4f7983a";
 
@@ -27,6 +29,22 @@ window.setTimeout(function () {
   document.getElementById("messages").appendChild(msg);
   out = document.getElementById("output");
   out.innerHTML = `${resp}   ${user}`;
+
+  var lag = setInterval(function () {
+    voices = speechSynthesis.getVoices();
+
+    if (voices.length !== 0) {
+      msg = new SpeechSynthesisUtterance();
+      msg.voice = voices[4];
+      console.log(voices);
+      msg.lang = "en-US";
+      speechSynthesis.speak(msg);
+      clearInterval(lag);
+      msg.text = resp + user;
+      speechSynthesis.speak(msg);
+    }
+  }, 200);
+  lag;
 }, 2000);
 
 input = document.getElementById("input");
@@ -92,6 +110,7 @@ function response() {
          <span class="time-left">${d.getHours() + ":" + d.getMinutes()}</span>
          `;
     out.innerHTML = resp;
+    speak();
   } else if (inp.includes("who are you")) {
     resp = "I am ASUNA.. the AI bot";
     msg.innerHTML = `
@@ -99,13 +118,16 @@ function response() {
          <p>${resp}</p>
          <span class="time-left">${d.getHours() + ":" + d.getMinutes()}</span>
     `;
+    speak();
   } else if (inp.includes("time")) {
     resp = "The Time is : ";
+    abc = `${d.getHours() + ":" + d.getMinutes()};`;
     msg.innerHTML = `
          <img src="bot2.webp" alt="Avatar" style="width:100%;">
-         <p>${resp} ${d.getHours() + ":" + d.getMinutes()}</p>
-         <span class="time-left">${d.getHours() + ":" + d.getMinutes()}</span>
+         <p>${resp}  ${abc} </p>
+         <span class="time-left">${abc}</span>
     `;
+    speak();
   } else if (inp.includes("date")) {
     resp = "sorry i have a headache";
     msg.innerHTML = `
@@ -113,35 +135,40 @@ function response() {
          <p>${resp}</p>
          <span class="time-left">${d.getHours() + ":" + d.getMinutes()}</span>
     `;
+    speak();
   } else if (inp.includes("joke")) {
     jokes();
   } else if (inp.includes("are u single")) {
-    resp = "I am in relationship with wifi";
+    resp = "I am in relationship with my wifi";
     msg.innerHTML = `
          <img src="bot2.webp" alt="Avatar" style="width:100%;">
          <p>${resp}</p>
          <span class="time-left">${d.getHours() + ":" + d.getMinutes()}</span>
     `;
-  } else if (inp.includes("weather")) {
-    getdata();
-    inp.value = " ";
-  } else if (inp.includes("bye")) {
+    speak();
+    // } else if (inp.includes("weather")) {
+    //   getdata();
+    //   inp.value = " ";
+    // } else if (inp.includes("bye")) {
     resp = `Bye ${user}.. Have a Good Day !!`;
     msg.innerHTML = `
          <img src="bot2.webp" alt="Avatar" style="width:100%;">
          <p>${resp}</p>
          <span class="time-left">${d.getHours() + ":" + d.getMinutes()}</span>
     `;
+    speak();
+    lag;
   } else if (inp.includes("play")) {
     Name = document.getElementById("input").value;
     Name = Name.slice(5, Name.length);
-    location.assign("https://www.youtube.com/results?search_query=" + Name);
     resp = `playing ${Name} on youtube`;
     msg.innerHTML = `
          <img src="bot2.webp" alt="Avatar" style="width:100%;">
          <p>${resp}</p>
          <span class="time-left">${d.getHours() + ":" + d.getMinutes()}</span>
     `;
+    speak();
+    location.assign("https://www.youtube.com/results?search_query=" + Name);
   } else {
     resp = "I don't get it can You repeat again ";
     msg.innerHTML = `
@@ -150,6 +177,7 @@ function response() {
          <span class="time-left">${d.getHours() + ":" + d.getMinutes()}</span>
     `;
     out.innerHTML = resp;
+    speak();
   }
 }
 
@@ -162,41 +190,62 @@ async function jokes() {
          <p>${resp}</p>
          <span class="time-left">${d.getHours() + ":" + d.getMinutes()}</span>
     `;
+  speak();
 }
-function getLocation() {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(showPosition);
-  } else {
-    x.innerHTML = "Geolocation is not supported by this browser.";
-  }
-}
-function showPosition(position) {
-  // x.innerHTML = "Latitude: " + position.coords.latitude +
-  // "<br>Longitude: " + position.coords.longitude;
-  lat = position.coords.latitude;
-  lon = position.coords.longitude;
+// function getLocation() {
+//   if (navigator.geolocation) {
+//     navigator.geolocation.getCurrentPosition(showPosition);
+//   } else {
+//     x.innerHTML = "Geolocation is not supported by this browser.";
+//   }
+// }
+// function showPosition(position) {
+//   // x.innerHTML = "Latitude: " + position.coords.latitude +
+//   // "<br>Longitude: " + position.coords.longitude;
+//   lat = position.coords.latitude;
+//   lon = position.coords.longitude;
 
-  lat = lat;
-  lon = lon;
+//   lat = lat;
+//   lon = lon;
 
-  getdata();
-}
-getLocation();
+//   getdata();
+// }
+// getLocation();
 
-async function getdata() {
-  let response2 = await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?&lat=${lat}&lon=${lon}&appid=${apikey}&units=metric`
-  );
-  data = await response2.json();
-  resp = `Today's weather of City : ${data.name} is  ${data.main.temp}°C`;
-  msg = document.createElement("div");
-  msg.classList.add("container");
-  d = new Date();
-  msg.innerHTML = `
-         <img src="bot2.webp" alt="Avatar" style="width:100%;">
-         <p>${resp}</p>
-         <span class="time-left">${d.getHours() + ":" + d.getMinutes()}</span>
-    `;
-}
+// async function getdata() {
+//   let response2 = await fetch(
+//     `https://api.openweathermap.org/data/2.5/weather?&lat=${lat}&lon=${lon}&appid=${apikey}&units=metric`
+//   );
+//   data = await response2.json();
+//   resp = `Today's weather of City : ${data.name} is  ${data.main.temp}°C`;
+//   msg = document.createElement("div");
+//   msg.classList.add("container");
+//   d = new Date();
+//   msg.innerHTML = `
+//          <img src="bot2.webp" alt="Avatar" style="width:100%;">
+//          <p>${resp}</p>
+//          <span class="time-left">${d.getHours() + ":" + d.getMinutes()}</span>
+//     `;
+
+//   // lag;
+// }
 
 // location.assign("https://www.youtube.com/results?search_query=" + name);
+
+function speak() {
+  var lag = setInterval(function () {
+    voices = speechSynthesis.getVoices();
+
+    if (voices.length !== 0) {
+      msg = new SpeechSynthesisUtterance();
+      msg.voice = voices[4];
+      msg.lang = "en-US";
+      speechSynthesis.speak(msg);
+      clearInterval(lag);
+      msg.text = resp;
+      speechSynthesis.speak(msg);
+    }
+  }, 200);
+  lag;
+  console.log("speakingggg");
+}
